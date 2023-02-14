5f11fa5bcb2e74de3d2765ff77c6338a8e720d2d
mdrakibulislam10 <mdrakibulislam752@gmail.com>
2023-02-14 18:21:49 +0600

    login page

--- js/login-utilities.js
+++ js/login-utilities.js
@@ -8,7 +8,7 @@ function getInputValue(inputId) {
 // validation and go to another page
 function validation(name, email, password) {
     if ((name) && (email.includes("@") && email.endsWith(".com")) && password) { // if(name / password is  defined);
-        window.location.href = "banking.html";
+        window.location.href = "banking/banking.html";
         return;
     }
     alert("please enter Name, Email and Password");
